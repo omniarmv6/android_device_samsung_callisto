e9918a64f957e867c8fdaa290e36428c7b51e782
Conn O'Griofa <connogriofa@gmail.com>
2013-09-21 14:13:59 +0100

    Initial callisto support (second try)
    * Used tass LDPI repository as reference for initial commit
    * Override BOARD_BML_BOOT and BOARD_BML_RECOVERY to use proper
      partitions (bml9 and bml10, respectively)
    * Install custom parts.rc to ensure that proper Bluetooth firmware
      is used (BCM2049B0)
    
    Change-Id: I3cd45aa69354cdba5e347ca90427482c4b982169

---
+++ BoardConfig.mk
@@ -0,0 +1,35 @@
+# Copyright (C) 2007 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# BoardConfig.mk
+#
+# Product-specific compile-time definitions.
+#
+
+include device/samsung/msm7x27-common/BoardConfigCommon.mk
+
+## Kernel, bootloader
+TARGET_BOOTLOADER_BOARD_NAME := callisto
+TARGET_KERNEL_CONFIG := cyanogenmod_callisto_defconfig
+TARGET_OTA_ASSERT_DEVICE := callisto,GT-I5510
+
+## Recovery
+BOARD_BML_BOOT := "/dev/block/bml9"
+BOARD_BML_RECOVERY := "/dev/block/bml10"
+BOARD_LDPI_RECOVERY := true
+BOARD_USE_CUSTOM_RECOVERY_FONT := "<font_7x16.h>"
+
+## Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/callisto/bluetooth
+
---
+++ device_callisto.mk
@@ -0,0 +1,41 @@
+# Copyright (C) 2009 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+## Note: this file and all of its includes are parsed before any
+## BoardConfig files. 3.0 Kernel & ath6kl configuration must be set here.
+
+## Build the 3.0.x Kernel
+#BUILD_WITH_30X_KERNEL := true
+
+## Build the ath6kl-compat driver
+#BOARD_WLAN_DEVICE := ath6kl_compat
+
+## Ramdisk
+PRODUCT_COPY_FILES += \
+    device/samsung/callisto/ramdisk/init.gt-i5510board.parts.rc:root/init.gt-i5510board.parts.rc \
+    device/samsung/callisto/ramdisk/CALLISTO.rle:root/CALLISTO.rle
+
+# Inherit products (Most specific first)
+# callisto blobs > samsung common(device/vendor) > other blobs
+$(call inherit-product, vendor/samsung/callisto/vendor_blobs.mk)
+$(call inherit-product, device/samsung/msm7x27-common/common.mk)
+$(call inherit-product, vendor/samsung/msm7x27-common/vendor.mk)
+
+## LDPI assets
+PRODUCT_AAPT_CONFIG := normal mdpi ldpi
+PRODUCT_AAPT_PREF_CONFIG := ldpi
+$(call inherit-product, device/ldpi-common/ldpi.mk)
+
+## Inherit overlays  (Most specific last)
+DEVICE_PACKAGE_OVERLAYS += device/samsung/callisto/overlay
